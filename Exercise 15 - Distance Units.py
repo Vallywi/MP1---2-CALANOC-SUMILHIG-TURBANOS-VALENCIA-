c4813fe96a876446df3342c52088beb88f03b44f
Valiwri <jecynvallirieturbanos@gmail.com>
2026-04-25 14:48:56 -1200

    Just updated the repo

--- Exercise 15 - Distance Units.py
+++ Exercise 15 - Distance Units.py
@@ -8,5 +8,6 @@ print ("Feet to inches: ", feet_inches)
 feet_yards = measurement / 3
 print (f"Feet to yards: {feet_yards:.4f}")
 
+
 feet_miles = measurement / 5280
 print (f"Feet to miles: {feet_miles:.4f}")
